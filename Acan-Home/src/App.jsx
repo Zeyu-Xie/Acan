7387f9244dcc1dade11c1bee8c1e67e40e75e033
Acan <acanthuridae@foxmail.com>
2023-07-08 23:55:17 +0800

    .

--- Acan-Home/src/App.jsx
+++ Acan-Home/src/App.jsx
@@ -1,17 +1,14 @@
 import React from "react"
 
 import "./App.css"
-
 import cafe from "./cafe.jpg"
-
 import config from "./config.json"
 
 class App extends React.Component {
     render() {
         return (
             <div className="wp-site-blocks">
-                <main className="wp-block-group is-layout-flow wp-container-8 wp-block-group-is-layout-flow"
-                    id="wp--skip-link--target">
+                <main className="wp-block-group is-layout-flow wp-container-8 wp-block-group-is-layout-flow" id="wp--skip-link--target">
                     <div className="wp-block-group alignfull has-global-padding is-layout-constrained wp-block-group-is-layout-constrained my-0">
                         <div className="wp-block-columns alignfull is-layout-flex wp-container-3 wp-block-columns-is-layout-flex">
                             <div className="wp-block-column is-layout-flow wp-block-column-is-layout-flow px-0">
@@ -19,46 +16,50 @@ class App extends React.Component {
                                     <img decoding="async" src={cafe} alt="Acan Home" />
                                 </figure>
                             </div>
-                            <div className="wp-block-column is-vertically-aligned-bottom has-global-padding is-layout-constrained wp-container-2 wp-block-column-is-layout-constrained"
-                                style={{ paddingRight: "var(--wp--preset--spacing--50)", paddingLeft: "var(--wp--preset--spacing--50)", flexBasis: "33.5%" }}>
-                                <h1 className="wp-block-heading"
-                                    style={{ fontSize: "max(48px, 5.5vw)", fontStyle: "normal", fontWeight: "700", lineHeight: "0.9" }}>
-                                    Acan<br />Website
+                            <div id="central-header-box" className="wp-block-column is-vertically-aligned-bottom has-global-padding is-layout-constrained wp-container-2 wp-block-column-is-layout-constrained" style={{ flexBasis: "33.5%" }}>
+                                <h1 id="central-header" className="wp-block-heading">
+                                    Acan
+                                    <br />
+                                    Website
                                 </h1>
                             </div>
                         </div>
                         <div className="wp-block-columns alignfull is-layout-flex wp-container-6 wp-block-columns-is-layout-flex">
-                            <div className="wp-block-column has-global-padding is-content-justification-right is-layout-constrained wp-container-4 wp-block-column-is-layout-constrained"
-                                style={{ paddingRight: "var(--wp--preset--spacing--50)", paddingLeft: "var(--wp--preset--spacing--50)", flexBasis: "67%" }}>
+                            <div id="central-quote" className="wp-block-column has-global-padding is-content-justification-right is-layout-constrained wp-container-4 wp-block-column-is-layout-constrained">
                                 <p>
                                     Welcome to Acan's personal website, where I display my works and share my life. I am a
                                     student from Tsinghua University, majoring in Mathematics but dreaming of becoming a
                                     programmer. I hope my passion and effort can be seen from this website.
                                 </p>
                             </div>
-                            <div className="wp-block-column has-global-padding is-content-justification-left is-layout-constrained wp-container-5 wp-block-column-is-layout-constrained"
-                                style={{ paddingRight: "var(--wp--preset--spacing--50)", paddingLeft: "var(--wp--preset--spacing--50)", flexBasis: "33%" }}>
+                            <div id="unknown-block" className="wp-block-column has-global-padding is-content-justification-left is-layout-constrained wp-container-5 wp-block-column-is-layout-constrained">
+
                             </div>
                         </div>
-                        <div style={{ height: "2rem" }} aria-hidden="true" className="wp-block-spacer"></div>
+                        <div id="central-padding" aria-hidden="true" className="wp-block-spacer">
+
+                        </div>
                     </div>
                 </main>
                 <footer className="wp-block-template-part">
-                    <div className="wp-block-group alignfull has-global-padding is-layout-constrained wp-block-group-is-layout-constrained"
-                        style={{ paddingTop: "var(--wp--preset--spacing--80)" }}>
-                        <div className="wp-block-group alignfull is-content-justification-space-between is-nowrap is-layout-flex wp-container-10 wp-block-group-is-layout-flex"
-                            style={{ paddingTop: "var(--wp--preset--spacing--60)", paddingBottom: "var(--wp--preset--spacing--60)" }}>
-                            <div className="wp-block-group is-nowrap is-layout-flex wp-container-9 wp-block-group-is-layout-flex"
-                                style={{ paddingRight: "var(--wp--preset--spacing--50)", paddingLeft: "var(--wp--preset--spacing--50)" }}>
-                                <h1 className="wp-block-site-title">
-                                    <a href="#" target="_self"
-                                        rel="home" aria-current="page" style={{ width: "max-content" }}>Acan Xie
-                                    </a>
-                                </h1>
-                                <p className="has-x-small-font-size"><a href={config.urls.github}>GitHub</a> / <a href={config.urls.instagram}>Instagram</a> / <a
-                                    href={config.urls.email}>Email</a></p>
-                            </div>
-                            <p className="has-text-align-right has-x-small-font-size">
+                    <div className="row px-3 pb-3 align-items-center">
+
+                        <div id="footer-title">
+                            <h1 className="wp-block-site-title">
+                                <a id="footer-title" href="#" target="_self" rel="home" aria-current="page">
+                                    Acan Xie
+                                </a>
+                            </h1>
+                        </div>
+
+                        <div id="footer-contact">
+                            <p className="has-x-small-font-size m-0">
+                                <a href={config.urls.github}>GitHub</a> / <a href={config.urls.instagram}>Instagram</a> / <a href={config.urls.email}>Email</a>
+                            </p>
+                        </div>
+
+                        <div className="col">
+                            <p className="has-text-align-right has-x-small-font-size m-0 text-end">
                                 <a href="#" rel="nofollow">
                                     Home
                                 </a>
