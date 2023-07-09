a17e416202096303c7d509ec3f6e7da9a23b7fcb
Acan <acanthuridae@foxmail.com>
2023-07-09 19:10:06 +0800

    .

--- Acan-Home/src/App.jsx
+++ Acan-Home/src/App.jsx
@@ -7,82 +7,54 @@ import config from "./config.json"
 class App extends React.Component {
     render() {
         return (
-            <div className="wp-site-blocks">
-                <main className="wp-block-group is-layout-flow wp-container-8 wp-block-group-is-layout-flow" id="wp--skip-link--target">
-                    <div className="wp-block-group alignfull has-global-padding is-layout-constrained wp-block-group-is-layout-constrained my-0">
-                        <div className="wp-block-columns alignfull is-layout-flex wp-container-3 wp-block-columns-is-layout-flex">
-                            <div className="wp-block-column is-layout-flow wp-block-column-is-layout-flow px-0">
-                                <figure className="wp-block-image m-0">
-                                    <img decoding="async" src={cafe} alt="Acan Home" />
-                                </figure>
-                            </div>
-                            <div id="central-header-box" className="wp-block-column is-vertically-aligned-bottom has-global-padding is-layout-constrained wp-container-2 wp-block-column-is-layout-constrained" style={{ flexBasis: "33.5%" }}>
-                                <h1 id="central-header" className="wp-block-heading">
-                                    Acan
-                                    <br />
-                                    Website
-                                </h1>
-                            </div>
-                        </div>
-                        <div className="wp-block-columns alignfull is-layout-flex wp-container-6 wp-block-columns-is-layout-flex">
-                            <div id="central-quote" className="wp-block-column has-global-padding is-content-justification-right is-layout-constrained wp-container-4 wp-block-column-is-layout-constrained">
-                                <p>
-                                    Welcome to Acan's personal website, where I display my works and share my life. I am a
-                                    student from Tsinghua University, majoring in Mathematics but dreaming of becoming a
-                                    programmer. I hope my passion and effort can be seen from this website.
-                                </p>
-                            </div>
-                            <div id="unknown-block" className="wp-block-column has-global-padding is-content-justification-left is-layout-constrained wp-container-5 wp-block-column-is-layout-constrained">
+            <div className="container-fluid">
 
-                            </div>
-                        </div>
-                        <div id="central-padding" aria-hidden="true" className="wp-block-spacer">
+                {/* row1 */}
 
-                        </div>
+                <div id="home-row1" className="row mb-4">
+                    <div id="home-photo-box" className="col-12 col-md-8 p-0">
+                        <img src={cafe} alt="Acan Home" />
                     </div>
-                </main>
-                <footer className="wp-block-template-part">
-                    <div className="row px-3 pb-3 align-items-center">
-
-                        <div id="footer-title">
-                            <h1 className="wp-block-site-title">
-                                <a id="footer-title" href="#" target="_self" rel="home" aria-current="page">
-                                    Acan Xie
-                                </a>
-                            </h1>
-                        </div>
-
-                        <div id="footer-contact">
-                            <p className="has-x-small-font-size m-0">
-                                <a href={config.urls.github}>GitHub</a> / <a href={config.urls.instagram}>Instagram</a> / <a href={config.urls.email}>Email</a>
-                            </p>
-                        </div>
-
-                        <div className="col">
-                            <p className="has-text-align-right has-x-small-font-size m-0 text-end">
-                                <a href="#" rel="nofollow">
-                                    Home
-                                </a>
-                                &nbsp;/&nbsp;
-                                <a href={`${config.urls["Acan Server"]}/page/doc`} rel="nofollow">
-                                    Doc
-                                </a>
-                                &nbsp;/&nbsp;
-                                <a href={`${config.urls["Acan Server"]}/page/photo`} rel="nofollow">
-                                    Photo
-                                </a>
-                                &nbsp;/&nbsp;
-                                <a href={`${config.urls["Acan Server"]}/page/tool`} rel="nofollow">
-                                    Tool
-                                </a>
-                                &nbsp;/&nbsp;
-                                <a href={`${config.urls["Acan Server"]}/page/user`} rel="nofollow">
-                                    User
-                                </a>
-                            </p>
-                        </div>
+                    <div id="home-header-box" className="d-flex col-12 col-md-4 align-items-end">
+                        <h1 className="mt-5 mb-0">
+                            Acan
+                            <br />
+                            Website
+                        </h1>
                     </div>
-                </footer>
+                </div>
+
+                {/* row2 */}
+
+                <div id="home-row2" className="row justify-content-center">
+                    <div className="col-12 col-md-6">
+                        <p id="central-text" className="text-wrap">
+                            {config.content["central-text"]}
+                        </p>
+                    </div>
+                </div>
+
+                {/* row3 */}
+
+                <div id="home-row3" className="row">
+
+                    <div id="bottom-nav-header-box">
+                        <h2 className="m-0">
+                            <a href="#">
+                                Acan Home
+                            </a>
+                        </h2>
+                    </div>
+
+                    <div id="bottom-nav-social-box">
+                        <a href={config.urls.github}>Github</a> / <a href={config.urls.instagram}>Instagram</a> / <a href={config.urls.email}>Email</a>
+                    </div>
+
+                    <div id="bottom-nav-menu-box" className="justify-self-end">
+                        <a href="#">Home</a> / <a href={`${config.urls["Acan Server"]}/page/doc`}>Doc</a> / <a href={`${config.urls["Acan Server"]}/page/photo`}>Photo</a> / <a href={`${config.urls["Acan Server"]}/page/tool`}>Tool</a> / <a href={`${config.urls["Acan Server"]}/page/user`}>User</a>
+                    </div>
+
+                </div>
             </div>
         )
     }
